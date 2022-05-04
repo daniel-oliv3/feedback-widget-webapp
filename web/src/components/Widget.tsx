7a394249d8e42227308e1c342963b39502e85dc4
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-04 12:33:19 -0300

    Update

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,5 +1,12 @@
 import { ChatTeardropDots } from "phosphor-react"
 
 export function Widget(){
-    return <h1><ChatTeardropDots/></h1>
+    return (
+        <div className="absolute bottom-5 right-5">
+            <button className="bg-brand-500 rounded-full px-3 h-12 text-white">
+                <ChatTeardropDots className="w-6 h-6" />
+                <span>Feedback</span>
+            </button>
+        </div>
+    )
 }
